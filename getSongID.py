1ae7833d5c2e809ad39ae510000ea52aef211bc8
Rifad Lafir <rifad.lafir@student.manchester.ac.uk>
2017-03-04 12:42:59 +0000

    added spotify IDs to the JSON

---
+++ getSongID.py
@@ -0,0 +1,31 @@
+import spotipy
+import json 
+
+#create spotify object
+spotify = spotipy.Spotify()
+
+#open songs.json
+with open('songs.json','r') as infile:
+  songs = json.load(infile)
+
+for song in songs:
+  name = song["song"]+ " Ed Sheeran"
+  
+  results = spotify.search(q=name,limit=1)
+  
+  dumpID = results["tracks"]["items"][0]["id"]
+  dumpArtist = results["tracks"]["items"][0]["artists"][0]["name"]
+
+  song["spotifyID"] = dumpID
+
+  print song["song"]
+  print dumpArtist
+  print dumpID
+  
+
+
+with open('songs.json','w') as outfile:
+  json.dump(songs,outfile,sort_keys=True,indent=2)
+
+
+ 
